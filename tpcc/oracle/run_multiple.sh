edf51ea88be791e881428b0230eca37573bfe3fa
Maksim Zinal <zinal@ydb.tech>
2025-03-02 18:48:19 +0300

    avoid sleeping after the last run

--- tpcc/oracle/run_multiple.sh
+++ tpcc/oracle/run_multiple.sh
@@ -2,7 +2,7 @@
 
 log_dir="$HOME/tpcc_logs/oracle"
 whlist="1000 2000 3000 4000"
-sleep_minutes=10
+sleep_minutes=1
 
 args=()
 
@@ -20,14 +20,20 @@ esac; shift; done
 
 mkdir -p $log_dir
 
+first_run=1
+
 for wh in $whlist; do
+    if [ "$first_run" = "1" ]; then
+        first_run=0
+    else
+        # we need this to make sure that the next run will start with a clean state
+        # as well as have easily separable graphs
+        sleep ${sleep_minutes}m
+    fi
+
     dt=`date +%Y%m%d_%H%M`
     log_file="$log_dir/run_${dt}_${wh}wh.log"
     echo "Logging into $log_file"
 
     ./run_oracle.sh --warehouses $wh ${args[@]} 2>&1 | tee $log_file
-
-    # we need this to make sure that the next run will start with a clean state
-    # as well as have easily separable graphs
-    sleep ${sleep_minutes}m
 done
